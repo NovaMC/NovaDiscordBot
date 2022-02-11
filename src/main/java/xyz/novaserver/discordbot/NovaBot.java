493d310eb01c70ebbd9aa31eeebcb9e699f41a85
Drfatnipples <greavessimmie@gmail.com>
2022-02-11 04:13:28 -0600

    create config and bot login

--- src/main/java/xyz/novaserver/discordbot/NovaBot.java
+++ src/main/java/xyz/novaserver/discordbot/NovaBot.java
@@ -1,21 +1,47 @@
 package xyz.novaserver.discordbot;
 
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.spongepowered.configurate.ConfigurationNode;
 import xyz.novaserver.discordbot.util.Config;
 
+import javax.security.auth.login.LoginException;
 import java.io.File;
 import java.util.concurrent.ScheduledExecutorService;
 
 public class NovaBot {
     public static final Logger LOGGER = LoggerFactory.getLogger(NovaBot.class);
-    public static final Config CONFIG = new Config(NovaBot.class, new File(System.getProperty("user.dir"), "config.yml"), "config.yml");
+    public static ConfigurationNode CONFIG;
 
     private static ScheduledExecutorService generalThreadPool;
     private static JDA jda;
 
     public static void main(String[] args) {
+        try {
+            CONFIG = Config.getConfig(NovaBot.class, new File(System.getProperty("user.dir"), "config.yml"), "config.yml").getRoot();
+            jda = JDABuilder.createDefault(CONFIG.node("bot-token").getString()).build();
+        } catch (NullPointerException e) {
+            LOGGER.error("Failed to load config! Make sure you have write permissions.", e);
+            System.exit(1);
+        } catch (LoginException e) {
+            LOGGER.error("Failed to login to the bot. Did you change the bot token?", e);
+            System.exit(1);
+        }
+
+
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
